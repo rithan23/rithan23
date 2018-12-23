06644c3de7771327575ecae8798733abdcfa76ac
rithan23 <46054674+rithan23@users.noreply.github.com>
2018-12-23 10:45:41 -0800

    Create set1-2.c

---
+++ set1-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a;
+clrscr();
+printf("enter a number");
+scanf("%d",&n);
+if(a%2==0)
+{
+printf("it is even");
+}
+if(a%2!=0)
+{
+printf("it is odd");
+}
+else
+{
+printf("it is invalid");
+}
+getch();
+}
