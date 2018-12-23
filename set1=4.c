2578a92196e041fb79dcd87de58f07c68f922329
rithan23 <46054674+rithan23@users.noreply.github.com>
2018-12-23 11:12:10 -0800

    Create set1=4.c

---
+++ set1=4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter n");
+scanf("%d",&n);
+if(n<=z&&n>=a)
+{
+printf("alphabet");
+}
+else if
+{
+printf("it is not a alphabet");
+}
+else
+{
+printf("invalid");
+}
+getch();
+}
