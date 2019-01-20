aa1c8ff6a8bb352c981e88e6912d7808a24aa797
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-01-20 07:49:45 -0800

    Create set2-6.c

---
+++ set2-6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i,j,flag;
+    clrscr();
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        flag=0;
+        for(j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+    {
+        printf("%d\t",i);
+    }
+    }
+    
+    getch();
+}
