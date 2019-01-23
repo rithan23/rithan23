5df44bf49418c57aeaf4456e16daee1ca16f998a
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-01-23 15:57:45 +0530

    Create pla1-2.c

---
+++ pla1-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int fact=1,i,n;
+   clrscr();
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       fact=fact*i;
+   }
+   printf("%d",fact);
+   getch();
+}
+  
