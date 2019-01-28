de2d1378f2159807b78b3d12ca254d3777373461
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-01-28 06:21:21 -0800

    Create set4-9.c

---
+++ set4-9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,c,x;
+clrscr();
+printf("\narray elements");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+c=a[0];
+for(i=0;i<10;i++)
+{
+if(a[i]>c)
+{
+c=a[i];
+}
+}
+printf("\n%d is highest",c);
+getch();
+
+}
