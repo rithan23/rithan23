4972cbb1e1cd626c9acd8964813e99188777d0fc
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-07 09:40:43 -0800

    Create set9-6.c

---
+++ set9-6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[10];
+	int i,check=0,j;
+	scanf("%s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		for(j=i+1;a[j]!=0;j++)
+		{
+			if(a[i]==a[j])
+			{
+				printf("no");
+				check=1;
+				break;
+			}
+		}
+		if(check==1)
+		break;
+	}
+
+if(check==0)
+{
+	printf("yes");
+}
+	return 0;
+  
+}
