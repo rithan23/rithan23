209b0ed87e33fcf8b401e78fe841ec8511f03e7c
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-07 09:24:13 -0800

    Create set8-8.c

---
+++ set8-8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    if(num%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("not");
+    }
+    return 0;
+}
