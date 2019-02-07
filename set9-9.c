c25ef28b945637d3242098707db46311cc613a11
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-07 09:43:57 -0800

    Create set9-9.c

---
+++ set9-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100];
+    int i,j,max;
+    scanf("%s",a);
+    max=a[0];
+    for(i=0;a[i]!='\0';i++)
+{
+for(j=i+1;a[j]!='\0';j++)
+{
+if(a[i]>a[j])
+{
+max=a[i];
+a[i]=a[j];
+a[j]=max;
+}
+}
+}
+    printf("%s",a);
+    return 0;
+    
+}
