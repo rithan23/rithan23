fb915ffb201a91022c43c84509a0864562e62541
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-07 09:31:00 -0800

    Create set9-1.c

---
+++ set9-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int kabali,opponent;
+    scanf("%d %d",&kabali,&opponent);
+    if(kabali<opponent)
+    {
+        kabali=opponent-kabali;
+    }
+    else
+    {
+        kabali=kabali-opponent;
+    }
+    printf("%d",kabali);
+
+    return 0;
+    
+}
