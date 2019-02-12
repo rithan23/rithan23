1b125e8519c6fe9a1ecb871ee40db953b7eacde8
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-12 15:39:47 +0530

    Create set11-5.c

---
+++ set11-5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    scanf("%d",&a);
+    printf("\n");
+    scanf("%d",&b);
+    printf("\n");
+    printf("%d%d",a,b);
+    getch();
+}
