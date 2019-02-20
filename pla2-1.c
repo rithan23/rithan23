68066e122871e41752933e261ade6216d4554d3d
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-20 15:52:24 +0530

    Create pla2-1.c

---
+++ pla2-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   char n[30];
+   scanf("%s",n);
+   if(n[0]=='s')
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+
+    return 0;
+    
+}
