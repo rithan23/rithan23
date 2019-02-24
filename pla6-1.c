0a8acce8da1fc456ff311e29a4d729ea974d8bd8
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-24 08:03:14 -0800

    Create pla6-1.c

---
+++ pla6-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    long int n,i,a[100],j,t;
+    scanf("%ld",&n);
+    if(1<= n<= 100000)
+    {
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&a[i]);
+    }
+    for(i=0;i<2;i++)
+    {
+     for(j=i+1;j<n;j++)
+     {
+         if(a[i]>a[j])
+         {
+             t=a[i];
+             a[i]=a[j];
+             a[j]=t;
+         }
+      }
+    }
+    printf("%ld",a[1]);
+    }
+return 0;
+
+}
