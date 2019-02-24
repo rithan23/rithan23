f9a967db21337b36a0c1f22783fdab6a44090d78
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-02-24 07:27:45 -0800

    Create pla2-9.c

---
+++ pla2-9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,flag=0;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i==2)
+            {
+                printf("%d ",i);
+            }
+            for(j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    flag=0;
+                    break;
+                
+                }
+                else
+                {
+                    flag=1;
+                }
+            }
+            if(flag==1)
+            {
+                printf("%d ",i);
+            }
+        }
+        
+    }
+
+    return 0;
+    
+}
