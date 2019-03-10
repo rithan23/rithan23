05e70dd2faf0e4dce910367e887bb41b4b10f1b5
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-10 11:15:26 -0700

    Create pla9-4.c

---
+++ pla9-4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,a[100],i,j,t,max=0;
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            t=a[i]|a[j];
+            if(t>max)
+            {
+                max=t;
+            }
+            
+        }
+    }
+    printf("%d",max);
+}
