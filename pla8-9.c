7ca295b0cb227c2297980187ec8e001942ca8b18
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-10 11:09:53 -0700

    Create pla8-9.c

---
+++ pla8-9.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    long int n,a[1000],i,j,d,max;
+    scanf("%ld",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%ld",&a[i]);
+    }
+    
+    max=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            d=a[i]-a[j];
+            if(max>d)
+            {
+                max=d;
+            }
+        }
+    }
+    if(max<0)
+    {
+        max=-max;
+    }
+    else
+    {
+        max=max;
+    }
+        printf("%ld ",max);
+}
