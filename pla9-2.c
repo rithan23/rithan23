8c2f16385dadf5609775e937b4630c6c6eef434e
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-10 11:13:31 -0700

    Create pla9-2.c

---
+++ pla9-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,a[100],i,j,t;
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    t=a[0];
+    for(i=0;i<n-1;i++)
+    {
+        {
+            t=(t&a[i+1]);
+        }
+    }
+    printf("%d",t);
+}
