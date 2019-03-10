f430e4b3a495a1672f5f93cbbbb2048dabc8e57b
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-10 10:49:38 -0700

    Create pla7-4.c

---
+++ pla7-4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,j,a[100],k;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    
+    if(1<=n<=100000)
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=1;i<k;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==a[j])
+                {
+                    printf("%d ",i);
+                }
+            }
+        }
+    }
+}
