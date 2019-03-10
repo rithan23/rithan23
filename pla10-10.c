5b424a58806bcf17c3c1687bfefa51007aea7853
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-10 11:33:14 -0700

    Create pla10-10.c

---
+++ pla10-10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    long int n;
+    
+    long int hex=0,rem,p=1;
+    scanf("%ld",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        hex=oct+rem*p;
+        p=p*2;
+        n=n/10;
+    }
+    printf("%lx",hex);
+}
