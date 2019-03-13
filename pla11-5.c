f52505797f25583c424866b5d356ff15af7fa35e
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-13 08:07:18 -0700

    Create pla11-5.c

---
+++ pla11-5.c
@@ -0,0 +1,69 @@
+
+#include<stdio.h>
+
+void main()
+
+{
+
+    int n,a[100000],b[1000000],i,j,k;
+    
+    printf("enter n");
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+
+    {
+
+    scanf("%d",&a[i]);
+
+    b[i]=a[i];
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+
+        for(j=i+1;j<n;j++)
+
+        {
+
+            if(a[i]>a[j])
+
+            {
+
+                k=a[j];
+
+                a[j]=a[i];
+
+                a[i]=k;
+
+            }
+
+        }
+
+    }
+
+    for(i=0;i<n;i++)
+    
+
+    {
+
+        for(j=0;j<n;j++)
+
+        {
+
+            if(a[i]==b[j])
+
+            {
+
+                printf("%d ",j+1);
+
+            }
+
+        }
+
+            }
+            
+}
