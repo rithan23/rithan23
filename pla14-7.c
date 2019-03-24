8499431c7a497e2d3a9d0d5f867531644aa761aa
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-24 00:32:43 -0700

    Create pla14-7.c

---
+++ pla14-7.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+    int n,rem,i,j=0,a[100],p;
+
+    scanf("%d",&n);
+
+    while(n)
+
+    {
+
+        rem=n%2;
+
+        a[j]=rem;
+        
+
+        j++;
+
+        n=n/2;
+
+    }
+
+    for(i=0;i<j;i++)
+
+    {
+
+        if(a[i]==1)
+
+        {
+
+            p=i;
+
+            break;
+
+        }
+
+    }
+
+    printf("%d",p+1);
+
+   }
