b3a92e5797b885aa3ca9b9f5c155849d7fc38de4
rithan23 <46054674+rithan23@users.noreply.github.com>
2019-03-24 00:25:49 -0700

    Create pla11-2.c

---
+++ pla11-2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+int num,a[100],s=0,i;
+
+scanf("%d",&num);
+
+for(i=0;i<num;i++)
+
+{
+
+scanf("%d",&a[i]);
+
+}
+
+for(i=0;i<num;i++)
+
+{
+
+s=s+a[i];
+
+}
+
+printf("%d",s);
+
+}
